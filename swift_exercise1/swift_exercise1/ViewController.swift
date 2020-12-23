91220dca033c64aa4e835aea5308ede1dd94e297
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2020-12-23 18:17:34 +0100

    add an empty project

---
+++ swift_exercise1/swift_exercise1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  swift_exercise1
+//
+//  Created by Thierry on 2020-12-24.
+//  Copyright © 2020 Thierry. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
