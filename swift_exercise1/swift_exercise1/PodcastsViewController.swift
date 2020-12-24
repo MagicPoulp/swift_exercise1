d9c6bb4e663203a984fe28d4b841263d64d72937
Thierry Vilmart <3144141+MagicPoulp@users.noreply.github.com>
2020-12-24 18:04:07 +0100

    success to dynamically add a view and change the stack's height

--- swift_exercise1/swift_exercise1/PodcastsViewController.swift
+++ swift_exercise1/swift_exercise1/PodcastsViewController.swift
@@ -11,7 +11,9 @@ import UIKit
 class PodcastsViewController: UIViewController {
 
     //MARK: Properties
-    @IBOutlet var podcastsScrollView: UIScrollView!
+    @IBOutlet var podcastsMainStackView: UIStackView!
+    
+    var podcastViewHeight: CGFloat = 500
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -30,13 +32,32 @@ class PodcastsViewController: UIViewController {
         DispatchQueue.global().async {
             // Fetch Image Data
             if let data = try? Data(contentsOf: url) {
+                // as said in the tutorial UI updates must be run on the main thread
                 DispatchQueue.main.async {
-                    // Create Image and Update Image View
-                    print("done")
-                    //self.imageView.image = UIImage(data: data)
+                    let numPodcasts = 3;
+                    self.podcastsMainStackView.addSubview(self.podcastView())
+                    let calculatedHeight: CGFloat = self.podcastViewHeight * CGFloat(numPodcasts)
+                    https://stackoverflow.com/questions/42669554/how-to-update-the-constant-height-constraint-of-a-uiview-programmatically
+                    for constraint in self.podcastsMainStackView.constraints {
+                        if constraint.identifier == "podcastsMainStackViewHeightConstraint" {
+                           constraint.constant = calculatedHeight
+                        }
+                    }
+                    self.podcastsMainStackView.layoutIfNeeded()
+                    
                 }
             }
         }
+    }
+    
+    // https://stackoverflow.com/questions/29094129/swift-creating-a-vertical-uiscrollview-programmatically
+    func podcastView() -> UIView {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.heightAnchor.constraint(equalToConstant: self.podcastViewHeight).isActive = true
+        view.widthAnchor.constraint(equalToConstant: 200).isActive = true
+        view.backgroundColor = .white
+        return view
     }
 }
 
